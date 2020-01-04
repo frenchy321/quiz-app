ea4f11d9b77cac32fe897737d5a8fec9cc739cf3
Andy <andy.french@hotmail.co.uk>
2020-01-04 14:33:32 +0000

    Imported UserService and Model to scores.ts

--- src/app/components/scores/scores.component.ts
+++ src/app/components/scores/scores.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "../../services/user.service";
+import { Users } from "../../models/Users";
+import { from } from "rxjs";
 
 @Component({
-  selector: 'app-scores',
-  templateUrl: './scores.component.html',
-  styleUrls: ['./scores.component.css']
+  selector: "app-scores",
+  templateUrl: "./scores.component.html",
+  styleUrls: ["./scores.component.css"]
 })
 export class ScoresComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
