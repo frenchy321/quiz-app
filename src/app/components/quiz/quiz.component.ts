9cdf4dfcb50f1a23b648ceb6e83c606dfcddde54
Andy <andy.french@hotmail.co.uk>
2020-01-19 17:39:28 +0000

    Altered currentQuestionIndex, selectedAnswer methods and added shuffle method

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { questions } from "./quiz.data";
+import { Router } from "@angular/router";
+import { UserService } from "../../services/user.service";
 
 export interface Question {
   questionText: string;
@@ -17,27 +19,51 @@ interface Answer {
 })
 export class QuizComponent implements OnInit {
   questions: Question[] = questions;
-  currentQuestionIndex: number | null;
-  selectedAnswer: Answer | null = null;
+  currentQuestionIndex: number | null = -1;
+  selectedAnswer: Answer | null;
   selectedAnswers: Answer[] = [];
 
-  constructor() {}
+  constructor(private router: Router, private userSevice: UserService) {}
 
   ngOnInit() {
+    this.questions = this.shuffle(this.questions);
     this.nextQuestion();
   }
 
   selectAnswer(answer: Answer) {
     if (this.selectedAnswer === null) {
       this.selectedAnswer = answer;
-      this.selectedAnswers[this.currentQuestionIndex] = answer;
+      this.selectedAnswers.push(answer);
     }
   }
 
   nextQuestion() {
-    this.currentQuestionIndex = Math.floor(
-      Math.random() * this.questions.length
-    );
+    this.currentQuestionIndex++;
     this.selectedAnswer = null;
+    if (this.currentQuestionIndex >= this.questions.length) {
+      const score =
+        this.selectedAnswers.filter(answer => {
+          return answer.isCorrect;
+        }).length * 2;
+      this.userSevice.submitScore(score);
+      this.router.navigate(["/scores"]);
+    }
+  }
+
+  shuffle(array) {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
   }
 }
